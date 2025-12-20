fb0a4d58ec153194951896a106db5b5cbd29d896
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-12-20 16:57:25 +0330

    fixes

--- lib/app.dart
+++ lib/app.dart
@@ -31,17 +31,15 @@ class FaalAppState extends State<FaalApp> {
     return MaterialApp(
         title: 'فال حافظ',
         theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
-            primarySwatch: Colors.amber,
-            fontFamily: 'Samim'),
+          primarySwatch: Colors.amber,
+          fontFamily: 'Samim',
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.amber,
+          ),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.amber,
+          ),
+        ),
         home: const HomePage(),
         builder: (BuildContext context, Widget? child) {
           return Directionality(
