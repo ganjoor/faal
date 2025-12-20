6c4b5ff12b72aa4894975f8df0c004876647355d
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-12-20 16:40:44 +0330

    fixes

--- lib/forms/homepage.dart
+++ lib/forms/homepage.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:after_layout/after_layout.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
@@ -86,6 +87,7 @@ class _HomePageState extends State<HomePage> with AfterLayoutMixin<HomePage> {
                     style: const TextStyle(
                       fontFamily: 'IranNastaliq',
                       fontSize: 28,
+                      height: 2.5,
                     ),
                   )),
               Visibility(
@@ -141,8 +143,8 @@ class _HomePageState extends State<HomePage> with AfterLayoutMixin<HomePage> {
 
     _curVerseOrder = 0;
 
-    _recitation =
-        _poem!.recitations[Random().nextInt(_poem!.recitations.length)];
+    _recitation = _poem!
+        .recitations[Random().nextInt(math.min(_poem!.recitations.length, 2))];
 
     if (_recitation!.verses == null) {
       setState(() {
@@ -264,23 +266,7 @@ class _HomePageState extends State<HomePage> with AfterLayoutMixin<HomePage> {
                             fit: BoxFit.cover),
                       ),
                       child: Center(
-                        // Center is a layout widget. It takes a single child and positions it
-                        // in the middle of the parent.
                         child: Column(
-                          // Column is also a layout widget. It takes a list of children and
-                          // arranges them vertically. By default, it sizes itself to fit its
-                          // children horizontally, and tries to be as tall as its parent.
-                          //
-                          // Invoke "debug painting" (press "p" in the console, choose the
-                          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                          // to see the wireframe for each widget.
-                          //
-                          // Column has various properties to control how it sizes itself and
-                          // how it positions its children. Here we use mainAxisAlignment to
-                          // center the children vertically; the main axis here is the vertical
-                          // axis because Columns are vertical (the cross axis would be
-                          // horizontal).
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: _verseWigets,
