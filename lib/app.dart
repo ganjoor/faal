021ca6b0288d24d8bc85cd571c9c916f601e0273
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-12-20 16:54:50 +0330

    refactoring

---
+++ lib/app.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart';
+
+import 'forms/homepage.dart';
+
+class FaalApp extends StatefulWidget {
+  const FaalApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => FaalAppState();
+}
+
+class FaalAppState extends State<FaalApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    if (kIsWeb) {
+      // Remove `loading` div
+      final loader = document.getElementsByClassName('loading');
+      if (loader.isNotEmpty) {
+        loader.first.remove();
+      }
+    }
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'فال حافظ',
+        theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.amber,
+            fontFamily: 'Samim'),
+        home: const HomePage(),
+        builder: (BuildContext context, Widget? child) {
+          return Directionality(
+            textDirection: TextDirection.rtl,
+            child: Builder(
+              builder: (BuildContext context) {
+                return MediaQuery(
+                  data: MediaQuery.of(context).copyWith(
+                    textScaler: const TextScaler.linear(1.0),
+                  ),
+                  child: child!,
+                );
+              },
+            ),
+          );
+        });
+  }
+}
